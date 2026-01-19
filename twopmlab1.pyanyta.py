8ef280da82284c1660f53999408f79ee0ccb0a22
Anyta <Anyta.t08@gmail.com>
2026-01-19 15:06:41 -0500

    Add files via upload

---
+++ twopmlab1.pyanyta.py
@@ -0,0 +1,16 @@
+#P1
+def myhelloworld():
+     print("Hello World")
+
+myhelloworld()
+#P2
+def input_output():
+     name=input("Enter your name: ")
+     age=int(input("Enter your age: "))
+     height=float(input("Enter your height:(in meters) "))
+    
+     print(f"Hello {name}")
+     print(f"You are {age} years old")
+     print(f"Your height is {height} meters")
+
+input_output()
